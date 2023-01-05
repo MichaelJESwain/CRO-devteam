d6e02586ec6d5559b8a90c6e912168bd817ac071
AnaCarolMendes <104446434+AnaCarolMendes@users.noreply.github.com>
2023-01-05 15:52:22 +0100

    Create boilerplate test A

---
+++ snippets/global/boilerplateTestA.js
@@ -0,0 +1,48 @@
+const Experiment = (() => {
+    //   Helpers
+    //   ================================================================================
+    const waitForElement = (cssSelector, cb) => {
+      var t1 = new Date().getTime()
+      var dif = 0
+      var reqAnId = window.requestAnimationFrame(lookForElement)
+  
+      function lookForElement() {
+        var t2 = new Date().getTime()
+        dif = (t2 - t1) / 1000
+  
+        if (dif < 3) {
+          if (elementFound()) {
+            cb(cssSelector)
+            cancelAnimationFrame(reqAnId)
+          } else {
+            window.requestAnimationFrame(lookForElement)
+          }
+        } else {
+          cancelAnimationFrame(reqAnId)
+        }
+      }
+  
+      function elementFound() {
+        var e = document.querySelector(cssSelector)
+        if (e) {
+          return true
+        } else {
+          return false
+        }
+      }
+    }
+  
+    //   ================================================================================
+  
+    const mainFunction = () => {
+        console.log("The experiment started")
+    }
+      
+    const start = () => {
+      mainFunction();
+    }
+  
+    return { start }
+  })()
+  
+Experiment.start()
