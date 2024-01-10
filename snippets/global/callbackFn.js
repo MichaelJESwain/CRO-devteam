8c403026a8a89b8055bff03c0377b4c0f5bb93d7
AnaCarolMendes <104446434+AnaCarolMendes@users.noreply.github.com>
2024-01-10 11:05:11 +0100

    Update activation function boilerplate

--- snippets/global/callbackFn.js
+++ snippets/global/callbackFn.js
@@ -14,3 +14,19 @@ function callbackFn(activate, options) {
     }
   }, 100)
 }
+
+// One more option:
+
+function callbackFn(activate, options) {
+  const isMWglp = new RegExp('/(men|women|heren|dames|herren|damen|femme|homme|donne|uomini|mujeres|hombres|kobiety|mezczyzni)$');
+  if (isMWglp.test(window.location.pathname)) {
+    optimizely.utils.waitUntil(() => {
+          return window &&
+          optimizely.get('visitor') &&
+          optimizely.get('visitor').events &&
+          document.querySelector('[data-testid="DynamicModule-component"]') !== undefined ;
+    }).then(() => {
+      activate();  
+    })
+  }
+}
