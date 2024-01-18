4a2162bce7245cc71189a344e78da4b20a743b29
AnaCarolMendes <104446434+AnaCarolMendes@users.noreply.github.com>
2024-01-18 13:45:53 +0100

    CK354 added

---
+++ experiments/CK/CK345 - Remind NL, PL users to choose their bank from dropdown in Payment page/README.md
@@ -0,0 +1,10 @@
+# HYPOTHESIS
+Because we learned that visitors (in NL & PL) trigger an error message in Checkout Payment, we expect that prompting them to select their bank from the Ideal (NL) and Dotpay (in PL) dropdown menus will support a frictionless checkout experience and result in an uplift on CVR. 
+
+## Variant 0 - Control
+No changes
+
+## Variant 1 
+Add a (red) copy box above the 'Select your bank' dropdown menu to remind users of selecting their bank in case they chose Ideal or Dotpay as a payment method
+
+### [Link](https://app.asana.com/0/1201109242799454/1204389469429308/f)
---
+++ experiments/CK/CK345 - Remind NL, PL users to choose their bank from dropdown in Payment page/activation.js
@@ -0,0 +1,37 @@
+function callbackFn(activate) {
+    var elementInterval;
+  
+    function check() {
+      if (
+        window &&
+        window.digitalData &&
+        window.__NEXT_DATA__.props.pageProps &&
+        window.next &&
+        window.next.router &&
+        window.next.router.events &&
+        typeof window.next.router.events.on === 'function' &&
+        typeof window.requestAnimationFrame === 'function' &&
+        document.querySelector('[data-testid="payments-wrapper"]') &&
+        document.querySelectorAll('[data-testid="paymentMethods-wrapper"] label') &&
+        (document.querySelector('[data-testid="ideal-payment-method"]') ||
+        document.querySelector('[data-testid="onlineBanking_PL-pvh-selectionItem"]')) &&
+        (document.querySelector('[data-testid="ideal-container"]') || 
+        document.querySelector('[data-testid="onlineBanking_PL-container"]')) 
+      ) {
+        clearInterval(elementInterval);
+  
+        if (window.next.router.route === '/checkout/payment') {
+            //console.log("active")
+          activate();
+        } else {
+          window.next.router.events.on('routeChangeComplete', function (route) {
+            if (route === '/checkout/payment') {
+              activate();
+            }
+          });
+        }
+      }
+    }
+
+    elementInterval = setInterval(check, 100);
+  }
