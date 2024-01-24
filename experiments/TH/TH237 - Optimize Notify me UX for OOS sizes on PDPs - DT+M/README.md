c0af58c9e0ad2571793e17fd9234e122de1db03f
AnaCarolMendes <104446434+AnaCarolMendes@users.noreply.github.com>
2024-01-24 09:53:20 +0100

    TH237 added

---
+++ experiments/TH/TH237 - Optimize Notify me UX for OOS sizes on PDPs - DT+M/README.md
@@ -0,0 +1,22 @@
+# HYPOTHESIS
+FBecause we learned that many visitors will not click on the 'notify me' CTA, we expect that by presenting a clear design of the size selector and motivating visitors to click through, we will increase the throughflow to the out of stock subscription form. This will result in additional out of stock subscriptions.
+
+
+## Variant 0 - Control
+When a OOS size is selected, CTA changes to Notify Me
+Click/tap triggers a pop up with an email field
+
+## Variant 1 
+Same as V0, but with an additional message before the CTA
+"This item is currently out of stock. Click the link below to be notified when it becomes available."
+Pop opens same as V0, no change in it
+
+## Variant 2
+Same as V1, but when a size is selected, the email field already exposed
+"This size is currently out of stock, but don’t worry! Enter your email address below and we’ll notify you when it becomes available again."
+No pop-up
+Success message after email input: "Thank you! We will email you when the item is back in stock."
+Success message disappears once user selects another size or refreshes the page
+
+
+### [Link](https://app.asana.com/0/1201109242799454/1203532814793877/f)
---
+++ experiments/TH/TH237 - Optimize Notify me UX for OOS sizes on PDPs - DT+M/V1/index.js
@@ -0,0 +1,48 @@
+const utils = window.optimizely.get('utils');
+const th237 = {
+  oosMessage: {
+  uk: 'This item is currently out of stock. Enter your email below to be notified when it becomes available.',
+  nl: 'Dit item is momenteel niet op voorraad. Vul je e-mailadres hieronder in en ontvang bericht wanneer het weer verkrijgbaar is.',
+  de: 'Dieser Artikel ist derzeit nicht auf Lager. Bitte gib deine E-Mail ein, wenn du benachrichtigt werden möchtest, sobald er wieder erhältlich ist.',
+  es: 'Este artículo está agotado. Introduce tu correo abajo y te avisaremos cuando vuelva a estar disponible.',
+  it: 'Questo articolo è attualmente esaurito. Inserisci il tuo indirizzo e-mail qui sotto e ti informeremo quando tornerà disponibile.',
+  fr: 'Cet article est actuellement en rupture de stock. Indiquez votre adresse e-mail ci-dessous pour recevoir une notification de retour en stock.',
+  pl: 'Ten produkt jest obecnie niedostępny. Wpisz poniżej swój adres e-mail – otrzymasz powiadomienie, kiedy będzie dostępny.'
+},
+    init: function() {
+      th237.createMessage();
+      th237.observeAddToBag();
+    },
+    observeAddToBag: function(){
+      utils.observeSelector('[data-testid="pdpActionButton-addToBag-pvh-button"]', function(){
+        if(document.querySelector('.exp-wrapper')){
+            let child = document.querySelector('.exp-wrapper');
+            document.querySelector('[data-testid="Right-GridItem"]').removeChild(child);
+        }
+        th237.init();
+    })
+  },
+    createMessage: function() {
+      utils.waitForElement('[data-testid="ProductActions-component"] [data-testid="pdpActionButton-notifyMe-pvh-button"]').then(function(){
+        const reference = document.querySelector('[data-testid="ProductActions-component"]'); 
+        const divWrapper = document.createElement("div");
+        if(!document.querySelector('.exp-wrapper')){
+          window.sharedTH237.placeBefore(divWrapper, reference );
+          divWrapper.classList.add('exp-wrapper');
+        }  
+        const language = window.location.hostname.split('.')[0];
+        const text = th237.oosMessage[language];
+        divWrapper.innerHTML = `<div class="exp-wrapper-stock">
+        <div class="exp-svg"><svg width="20" height="20" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M2.2 5.46503V16.3H17.8V5.46503L11.0821 11.6334C10.4701 12.1954 9.52985 12.1954 8.91785 11.6334L2.2 5.46503ZM16.8589 4.7L10.2705 10.7495C10.1175 10.89 9.88246 10.89 9.72946 10.7495L3.14107 4.7H16.8589ZM2 3.5C1.44772 3.5 1 3.94772 1 4.5V16.5C1 17.0523 1.44772 17.5 2 17.5H18C18.5523 17.5 19 17.0523 19 16.5V4.5C19 3.94772 18.5523 3.5 18 3.5H2Z" fill="#00174F"/>
+    </svg></div>
+    <p class="exp-text">${text}</p>
+        </div>`;
+      })
+      
+    },
+    
+};
+utils.observeSelector(`[data-testid="ProductSizeSelector-component"] [class*=ProductSizeSelector_SizeList]`, function(){
+th237.init();
+});
