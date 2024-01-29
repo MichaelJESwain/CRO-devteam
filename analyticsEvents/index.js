2b5a9375ad0aa1cfebbc6eea9d7ccd5411d92813
AnaCarolMendes <104446434+AnaCarolMendes@users.noreply.github.com>
2024-01-29 12:03:10 +0100

    Updated analyticsEvents folder

--- analyticsEvents/index.js
+++ analyticsEvents/index.js
@@ -7,6 +7,26 @@ addToBag.addEventListener('click', function () {
 
 // ======================================================
 
+// IntersectionObserver
+const observer = new window.IntersectionObserver(([entry]) => {
+    if (entry.isIntersecting) {
+        console.log(`do somenting`);
+        optimizely.sendAnalyticsEvents('<Event Name>');
+        return
+    }
+    //console.log('LEAVE')
+  }, {
+    root: null,
+    threshold: 0.4, // set offset 0.4 means trigger if atleast 40% of element in viewport
+  })
+  
+const element = document.querySelector('[data-testid="pdpActionButton-addToBag-pvh-button"]');
+observer.observe(element);
+
+
+
+
+// ======================================================  
 //Old version
 const {waitUntil} = window.optimizely.get('utils');
 
